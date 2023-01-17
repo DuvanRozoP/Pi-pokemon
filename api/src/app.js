6afa06046aa48690f65c475a6c08baf331925752
Duvanrozop <rozoparraduvan@gmail.com>
2023-01-16 19:14:20 -0500

    add comment routing

--- api/src/app.js
+++ api/src/app.js
@@ -23,7 +23,10 @@ server.use((req, res, next) => {
 });
 
 //server.use('/', routes);
+// * ingresando rutas
 routes(server);
+
+
 // Error catching endware.
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
