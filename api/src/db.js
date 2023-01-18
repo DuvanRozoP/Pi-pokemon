a5419cc9299a56944e69c3ab0a8ebb9f3e1ec321
Duvanrozop <rozoparraduvan@gmail.com>
2023-01-18 10:55:14 -0500

    between the table types and pokemon

--- api/src/db.js
+++ api/src/db.js
@@ -29,12 +29,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Pokemon, Type } = sequelize.models;
-//, Pokemon_type
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Pokemon.belongsToMany(Type, { through: 'Pokemon_type' });
-Type.belongsToMany(Pokemon, { through: 'Pokemon_type' });
+Pokemon.belongsToMany(Type, { through: 'pokemon_type' });
+Type.belongsToMany(Pokemon, { through: 'pokemon_type' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
