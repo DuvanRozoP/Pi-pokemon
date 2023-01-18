c9a964d3fb282c431624227c5e26bb743fabf8f5
Duvanrozop <rozoparraduvan@gmail.com>
2023-01-18 11:04:14 -0500

    adding routes

--- api/src/Components/Types/Types.network.js
+++ api/src/Components/Types/Types.network.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// !const { succes, error } = require('../../Network/response');
+const { succes, error } = require('../../Helpers/helpers');
+const { getTypesAllStore } = require('./Types.store');
 
-router.get('/', (request, response) => {
-  response.send('GET /types');
+router.get('/', async (request, response) => {
+  try {
+    succes(response, 200, await getTypesAllStore());
+  } catch (err) {
+    error(response, 401, err.message);
+  }
 });
 
 module.exports = router;
